6cff22eb3e4efd75b60c794026e98bc065dbf26b
Arthur Kalule <kalulearthur@gmail.com>
2020-01-13 08:24:44 +0300

    Correct env vars

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
-import * as process from 'process';
 
 export const environment = {
     production: true,
-    apiServerUrl: process.env.API_SERVER_URL, // the running FLASK api server url
+    apiServerUrl: 'https://ensemble-movies.herokuapp.com/api/v1', // the running FLASK api server url
     auth0: {
-        url: process.env.AUTH0_DOMAIN, // the auth0 domain prefix
-        audience: process.env.AUTH0_AUDIENCE, // the audience set for the auth0 app
-        clientId: process.env.AUTH0_CLIENTID, // the client id generated for the auth0 app
-        callbackURL: process.env.AUTH0_CALLBACKURL, // the base url of the running ionic application.
+        url: 'dev-7d54-828.auth0.com', // the auth0 domain prefix
+        audience: 'ensemble', // the audience set for the auth0 app
+        clientId: '3M60ex70HYUjaHBIDC9l1v0XOfT11hep', // the client id generated for the auth0 app
+        callbackURL: 'dev-7d54-828.auth0.com', // the base url of the running ionic application.
     }
 };
