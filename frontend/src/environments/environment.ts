90a58890ca330b245d0a4949dc9d11ff368bc27e
Arthur Kalule <kalulearthur@gmail.com>
2020-01-13 18:24:51 +0300

    Correct development config

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiServerUrl: 'http://127.0.0.1:4200/api/v1', // the running FLASK api server url
+    apiServerUrl: 'http://127.0.0.1:5000/api/v1', // the running FLASK api server url
     auth0: {
         url: 'dev-7d54-828.auth0.com', // the auth0 domain prefix
         audience: 'ensemble', // the audience set for the auth0 app
