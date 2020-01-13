83a6195b48146ce0ee44aa4ccc817e04ca97f46d
Arthur Kalule <kalulearthur@gmail.com>
2020-01-13 09:16:38 +0300

    update production env vars

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,9 +3,9 @@ export const environment = {
     production: true,
     apiServerUrl: 'https://ensemble-movies.herokuapp.com/api/v1', // the running FLASK api server url
     auth0: {
-        url: 'dev-7d54-828.auth0.com', // the auth0 domain prefix
-        audience: 'ensemble', // the audience set for the auth0 app
-        clientId: '3M60ex70HYUjaHBIDC9l1v0XOfT11hep', // the client id generated for the auth0 app
+        url: 'dev-008kxm8u.auth0.com', // the auth0 domain prefix
+        audience: 'ensemble-pro', // the audience set for the auth0 app
+        clientId: 'JoNFwlDztC4yJOcy4pLy4EOdH0yYagJ1', // the client id generated for the auth0 app
         callbackURL: 'https://ensemble-pro.herokuapp.com', // the base url of the running ionic application.
     }
 };
