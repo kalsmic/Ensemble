5df1866796e44a6a3d6f462d94f8223984de8e0a
Arthur Kalule <kalulearthur@gmail.com>
2020-01-23 18:31:49 +0300

    test: add more tests - add tests for http interceptor - add tests for toast service

--- frontend/src/app/core/toast.service.spec.ts
+++ frontend/src/app/core/toast.service.spec.ts
@@ -1,41 +1,49 @@
-import {CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from '@angular/core';
 import {TestBed} from '@angular/core/testing';
 import {ToastController} from '@ionic/angular';
+import {modalControllerSpy} from '../shared/__mocks__/index.mock';
 
 import {ToastService} from './toast.service';
 
 describe('ToastService', () => {
-  let service;
+    let service, showToastMock;
 
-  beforeEach(() => {
+    beforeEach(() => {
     TestBed.configureTestingModule({
-      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
       providers: [ToastController]
 
     });
     service = TestBed.get(ToastService);
+    showToastMock = jest.fn().mockResolvedValue('');
   });
 
-  it('should be created', () => {
+    afterEach(() => jest.clearAllMocks());
+
+    it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should run #success()', async () => {
-    service.showToast = jest.fn().mockReturnValue({
-      then() {
-      }
+    it('should run #showToast()', async () => {
+        service.toastController = modalControllerSpy;
+        await service.showToast('message', 'success');
+        expect(service.toastController.create).toHaveBeenCalled();
+        expect(service.toastController.create).toHaveBeenCalledWith({
+            color: 'success', duration: 3000, message: 'message', position: 'top'
+        });
     });
-    service.success({});
-    expect(service.showToast).toHaveBeenCalled();
+
+    it('should run #success()', async () => {
+        service.showToast = showToastMock;
+        service.success('success message');
+        expect(service.showToast).toHaveBeenCalled();
+        expect(service.showToast).toHaveBeenCalledWith('success message', 'tertiary');
+
   });
 
-  it('should run #error()', async () => {
-    service.showToast = jest.fn().mockReturnValue({
-      then() {
-      }
-    });
-    service.error({});
-    expect(service.showToast).toHaveBeenCalled();
+    it('should run #error()', async () => {
+        service.showToast = showToastMock;
+        service.error('error message');
+        expect(service.showToast).toHaveBeenCalled();
+        expect(service.showToast).toHaveBeenCalledWith('error message', 'danger');
   });
 
 });
