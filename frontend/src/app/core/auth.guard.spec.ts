6a0a14470feea81f70acdfc0c14da79e4b09c9dc
Arthur Kalule <kalulearthur@gmail.com>
2020-01-25 18:45:09 +0300

    test: add more tests - add tests for auth Guard

--- frontend/src/app/core/auth.guard.spec.ts
+++ frontend/src/app/core/auth.guard.spec.ts
@@ -1,18 +1,57 @@
-import {inject, TestBed} from '@angular/core/testing';
-import {Router} from '@angular/router';
-import {routerSpy} from '../shared/__mocks__/index.mock';
+import {getTestBed, inject, TestBed} from '@angular/core/testing';
+import {ActivatedRouteSnapshot, Router, RouterStateSnapshot} from '@angular/router';
+import {AuthServiceSpy, routerSpy} from '../shared/__mocks__/index.mock';
 
 import {AuthGuard} from './auth.guard';
+import {AuthService} from './auth.service';
+
 
 describe('AuthGuard', () => {
+    let
+        service: AuthGuard,
+        authService: AuthService,
+        router: Router,
+        state: RouterStateSnapshot,
+        route: ActivatedRouteSnapshot;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [AuthGuard, {provide: Router, useValue: routerSpy}]
+            providers: [
+                AuthGuard,
+                {provide: Router, useValue: routerSpy},
+                {provide: AuthService, useValue: AuthServiceSpy},
+            ]
+
         });
+        service = getTestBed().get(AuthGuard);
+        authService = getTestBed().get(AuthService);
+        router = getTestBed().get(Router);
+        route = new ActivatedRouteSnapshot();
+        state = {
+            url: 'authenticated/url',
+            root: route
+        };
+
+
     });
+    afterEach(() => jest.clearAllMocks());
 
     it('should ...', inject([AuthGuard], (guard: AuthGuard) => {
         expect(guard).toBeTruthy();
     }));
+
+    it('should run #canActivate() authenticated', async () => {
+        authService.isAuthenticated = jest.fn().mockReturnValue(true);
+        service.canActivate(route, state);
+        expect(authService.isAuthenticated).toHaveBeenCalled();
+        expect(router.navigate).not.toHaveBeenCalled();
+    });
+
+    it('should run #canActivate() redirect unauthenticated', async () => {
+        authService.isAuthenticated = jest.fn().mockReturnValue(false);
+        service.canActivate(route, state);
+        expect(authService.isAuthenticated).toHaveBeenCalled();
+        expect(router.navigate).toHaveBeenCalled();
+        expect(router.navigate).toHaveBeenCalledWith(['tabs/user-page',]);
+    });
 });
