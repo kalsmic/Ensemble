5fe6fa00443437c0fcf5c8abf2f69f044a8bfabf
Arthur Kalule <kalulearthur@gmail.com>
2020-01-28 20:07:21 +0300

    bug: fix missing Jwt module import
    - add JWT module to imports in the app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import {FormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouteReuseStrategy} from '@angular/router';
+import {JwtModule} from '@auth0/angular-jwt';
 
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
@@ -25,7 +26,9 @@ import {HttpErrorInterceptorService} from './services/http-error-interceptor.ser
         BrowserAnimationsModule,
         IonicModule.forRoot(),
         AppRoutingModule,
-        CoreModule
+        CoreModule,
+        JwtModule.forRoot({}),
+
     ],
     providers: [
         StatusBar,
